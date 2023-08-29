855993fc929d53d95b606532835d5ddfc060586c
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-08-29 19:20:39 -0300

    perf: update seeds.rb to make less database insert queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
 require 'logger'
 logger = ActiveSupport::TaggedLogging.new(Logger.new($stdout))
+started_on = Time.current
 
 last_ten_years = (2012..2023)
-quantity_of_students_per_class = (20..40)
 
 logger.tagged('STUDENTS') { logger.info 'Creating Students' }
 students = FactoryBot.create_list :student, 300
@@ -15,18 +15,27 @@ subjects = FactoryBot.create_list :subject, 7
 logger.tagged('TEACHERS') { logger.info 'Creating Teachers' }
 teachers = FactoryBot.create_list :teacher, 25
 
-def create_enrollments(course:, course_students:)
-  course_students.map do |student|
-    FactoryBot.create(:enrollment, student:, course:)
-  end
+def random_student_count_per_class
+  rand(20..40)
 end
 
-def create_exams_and_grades(enrollment:, course:, subject:)
-  quantity_of_exams = course.year == 2023 ? 4 : 8
-  quantity_of_exams.times do
-    exam = FactoryBot.create(:exam, course:, subject:)
-    FactoryBot.create(:grade, enrollment:, exam:)
+def create_enrollments(course_id:, students_ids:)
+  enrollments = students_ids.map do |student_id|
+    { code: SecureRandom.uuid, student_id:, course_id: }
   end
+  Enrollment.insert_all enrollments
+end
+
+def create_exams_and_grades(enrollment_code:, course:, subject:)
+  quantity_of_exams = course.year == 2023 ? 4 : 8
+
+  exams_arr = Array.new(quantity_of_exams) { FactoryBot.attributes_for(:exam, course_id: course.id, subject_id: subject.id)}
+  exams = Exam.insert_all exams_arr
+
+  grades = exams.map do |exam|
+    { exam_id: exam['id'], enrollment_code:, value: rand(0.0..10.0) }
+  end
+  Grade.insert_all grades
 end
 
 class_names = (5..9) # nth grade
@@ -34,21 +43,42 @@ class_names = (5..9) # nth grade
 last_ten_years.each do |year|
   class_names.each do |n|
     course = FactoryBot.create(:course, year:, name: "#{n}th grade")
+
     logger.tagged("YEAR=#{year}") { logger.info "Creating Classes for #{n}th grade" }
 
     teacher_subject_hash = teachers.sample(subjects.size).zip(subjects)
+
+    teacher_assignments = []
     teacher_subject_hash.each do |teacher, subject|
-      FactoryBot.create(:teacher_assignment, course:, subject:, teacher:)
+      teacher_id = teacher.id
+      course_id = course.id
+      subject_id = subject.id
+
+      teacher_assignments << { course_id:, subject_id:, teacher_id:}
 
       logger.tagged("YEAR=#{year}","#{n}th Grade") do
        logger.info "Creating classes for #{subject.name} with teacher #{teacher.name}"
       end
 
-      course_students = students.take(rand(quantity_of_students_per_class))
-      course_enrollments = create_enrollments(course:, course_students:)
-      course_enrollments.each do |enrollment|
-        create_exams_and_grades(enrollment:, course:, subject:)
+      students_ids = students.take(random_student_count_per_class).pluck(:id)
+      course_enrollments = create_enrollments(course_id:, students_ids:)
+      course_enrollments.map do |enrollment|
+        create_exams_and_grades(enrollment_code: enrollment['code'], course:, subject:)
       end
     end
+    TeacherAssignment.insert_all teacher_assignments
   end
+end
+
+ended_on = Time.current
+total_duration = (ended_on - started_on).in_milliseconds
+
+logger.tagged("SUCCESS") do
+  logger.info ("Finished in #{total_duration}ms")
+  logger.info ("Subjects: #{Subject.count}")
+  logger.info ("Teachers: #{Teacher.count}")
+  logger.info ("Students: #{Student.count}")
+  logger.info ("Courses: #{Course.count}")
+  logger.info ("Exams: #{Exam.count}")
+  logger.info ("Grades: #{Grade.count}")
 end
