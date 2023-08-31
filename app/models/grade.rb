33a632d468c746dfc16d5bd3bd465eeaf42d517f
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-08-31 13:31:47 -0300

    feat: create grades yearly_averages_by_subject scopa

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -6,4 +6,22 @@ class Grade < ApplicationRecord
 
   belongs_to :exam
   belongs_to :enrollment, foreign_key: :enrollment_code, inverse_of: :grades
+
+  scope :yearly_averages_by_subject, ->(year:) do
+    joins(enrollment: [:student, :course], exam: [:subject])
+    .where(course: { year: })
+    .select(
+      'subjects.name AS subject_name',
+      'enrollments.code AS enrollment_code',
+      'enrollments.code AS code',
+      'student.name AS student_name',
+      'course.year',
+      'AVG(grades.value) AS average',
+      'course.name AS course_name'
+    ).group('course.year', 'subjects.name', 'student.name', 'enrollments.code', 'course.name')
+  end
+
+  scope :find_by_student_id, ->(id:) do
+    where(student: { id: })
+  end
 end
