c853f8d592c5d0ed6e37a31a1be6267c449f8335
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-08 20:23:51 -0300

    feat: create enrollments_by_course and youngest_student_by_course

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
   before_action :set_year, only: [:show]
 
   def show
-    @enrollments_by_course = []
-    @youngest_student_by_course = []
+    @enrollments_by_course = Course.select_basic_fields.enrollments_count.by_year(year).order(name: :desc)
+    @youngest_student_by_course = Student.youngest_by_course.by_year(year).order_by_course_name
     @best_grades_by_subject = []
     @top_overloaded_teachers = []
   end
 
   private
+
+  attr_reader :year
 
   def set_year
     @year = params[:year] || Time.current.year
