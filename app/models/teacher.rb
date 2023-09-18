ca561420f42f554748250b8dabb0eaadeb4b1b12
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2023-09-18 14:42:08 -0300

    Revert "feat: create top_overloaded_teachers"
    This reverts commit d937635e076b8c4791a09b7b56efc6fa0a635288.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,16 +6,4 @@ class Teacher < ApplicationRecord
   has_many :teacher_assignments, dependent: :destroy
   has_many :courses, through: :teacher_assignments
   has_many :subjects, through: :teacher_assignments
-
-  scope :grouped_by_name, lambda {
-    select(:name)
-      .group(:name)
-  }
-
-  scope :top_overloaded, lambda { |year:|
-    joins(teacher_assignments: [course: [enrollments: :student]])
-      .select('COUNT(students.id) AS student_count')
-      .grouped_by_name
-      .where("courses.year = #{year}")
-  }
 end
