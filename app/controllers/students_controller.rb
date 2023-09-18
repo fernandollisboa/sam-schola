8c1022fd63cafe797836f3a4c322e32bdcb2d611
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2023-09-18 14:42:08 -0300

    Revert "feat: update students controller to calculate averages and show enrollment for the current year"
    This reverts commit f3a3f573285a4a6f3e1c535037e8f020a6084b09.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,44 +2,12 @@
 
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[show edit update destroy]
-  before_action :set_year, only: %i[index, show]
 
   def index
     @students = Student.all
   end
 
-  def show
-    @student = Student.resume(id: params[:id], year: @year)
-    .select(
-      :id,
-      :name,
-      :born_on,
-      "enrollments.code AS enrollment_code",
-      "courses.name AS course_name",
-      "AVG(grades.value) AS grades_average"
-    ).first
-
-    @grades = Grade.joins(enrollment: [:student, :course], exam: [:subject])
-    .where(course: { year: @year }, student: { id: 1 })
-    .select(
-      'subjects.name AS subject_name',
-      'enrollments.code AS enrollment_code',
-      'enrollments.code AS code',
-      'student.name AS student_name',
-      'course.year',
-      'AVG(grades.value) AS average',
-      'course.name AS course_name'
-    ).group('course.year', 'subjects.name', 'student.name', 'enrollments.code', 'course.name')
-
-
-    @enrollment_code = @student&.enrollment_code
-    @course_name = @student&.course_name
-    @grades_average = @student&.grades_average
-
-    if @student.nil?
-      set_student
-    end
-  end
+  def show; end
 
   def new
     @student = Student.new
@@ -74,10 +42,6 @@ class StudentsController < ApplicationController
 
   def set_student
     @student = Student.find(params[:id])
-  end
-
-  def set_year
-    @year = params[:year] || Date.current.year
   end
 
   def student_params
