ffd192fecf9e0a72d41a7238e152d0fe40e8ff36
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2023-09-18 14:42:08 -0300

    Revert "refactor: split student scopes in two"
    This reverts commit b361501bf54d01742df4d8b17019199e069d794c.

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,23 +8,9 @@ class Student < ApplicationRecord
   has_many :enrollments, dependent: :destroy
   has_many :grades, through: :enrollments
 
-  scope :yearly_reports, ->(year:) do
-    joins(enrollments: [:course])
-    .where(courses: { year: } )
-    .select(
-      :id,
-      :name,
-      :born_on,
-      'courses.year AS course_year',
-      'enrollments.code AS enrollment_code',
-      'courses.name AS course_name',
-    )
-    .distinct
-  end
-
-  scope :grade_averages, -> do
-    joins(enrollments: [:grades])
-    .select("AVG(grades.value) AS grades_average")
-    .group('enrollments.code', 'courses.name', 'students.id', "courses.year")
+  scope :resume, ->(id:, year:) do
+    joins(enrollments: [:course, :grades])
+    .where(id:, courses: { year: } )
+    .group('enrollments.code', 'courses.name', 'students.id')
   end
 end
