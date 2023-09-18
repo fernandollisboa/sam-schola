610244b830b7ff3e2a70515f8f5e77000d2b7cc7
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2023-09-18 14:42:08 -0300

    Revert "fix: broken view tests"
    This reverts commit f1662f8a9cc925eed54ff4496a2f4f97734112ae.

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
   describe "GET /show" do
-    pending "returns http success" do
+    it "returns http success" do
       get "/dashboard/show"
       expect(response).to have_http_status(:success)
     end
